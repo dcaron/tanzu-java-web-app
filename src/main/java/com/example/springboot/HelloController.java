5f057aa09c6b6e9aee992b6376c612019b838401
dcaron <dcaron@xs4all.nl>
2022-04-21 10:44:58 +0200

    parameterize

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,15 +1,17 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
+	@Value("${hello.greeting}")
+	private String greeting;
+
 	@RequestMapping("/")
 	public String index() {
-		
-		return "Greetings from Spring Boot + Tanzu!";
+		return greeting;
 	}
-
 }
